63a72e4fd0a54333b9360d6472918fff8082b7da
{Yuki <{yukiomura0309@gmail.com}>
2023-12-04 19:07:43 +0900

    Fix I2C frequency in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h>
-faf const uint32_t I2C_Freq = 4000000UL; // I2C 周波数
-const uint32_t I2C_Normal = 100000UL;    // I2C 周波数
+
+const uint32_t I2C_Freq = 4000000UL;  // I2C 周波数
+const uint32_t I2C_Normal = 100000UL; // I2C 周波数
 
 // GPIO I2C Setting
 const uint8_t I2C_SCL = 21; // GPIO21
